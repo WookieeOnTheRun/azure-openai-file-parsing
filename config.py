d8a6ec97f2e4dc9e402988a263cb20677722ac8b
Wookiee On the Run <24234792+WookieeOnTheRun@users.noreply.github.com>
2023-10-19 22:09:13 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,5 @@
-aoaiEndpoint = "https://golgothaopenai.openai.azure.com/"
-aoaiApiKey = "417b9038f40b4f589d632e061003ede8"
-aoaiModel = "golgotha-gpt-4-32k"
+aoaiEndpoint = ""
+aoaiApiKey = ""
+aoaiModel = "" # GPT-4 model, ideally 32K for larger JSON documents
 aoaiApiType = "azure"
 aoaiApiVersion = "2023-08-01-preview"
-
-saConnString = ""
-saContainer = ""
